e3c69c5251ce0282b7078536c586a989800d1eb1
Delan Azabani <delan@azabani.com>
2013-10-11 15:25:30 +0800

    add input command line handling

--- ucpcal.c
+++ ucpcal.c
@@ -1,5 +1,18 @@
 #include "ucpcal.h"
 
 int main(int argc, char **argv) {
-	return 0;
+	int return_value = 0;
+	char *default_filename = NULL;
+	switch (argc) {
+	case 1:
+		break;
+	case 2:
+		default_filename = argv[1];
+		break;
+	default:
+		fprintf(stderr, "Usage: %s [filename?]\n", argv[0]);
+		return_value = 1;
+		break;
+	}
+	return return_value;
 }
--- ucpcal.h
+++ ucpcal.h
@@ -1,6 +1,7 @@
 #ifndef UCPCAL_H
 #define UCPCAL_H
 
+#include <stdio.h>
 #include "gui.h"
 
 #endif
